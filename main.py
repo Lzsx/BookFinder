882af993a533bfc2ba8c02a7009e315763421a7d
Lzsx <luiz.fernandos1807@gmail.com>
2022-12-27 02:42:32 -0300

    Better logs for user

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import requests
 from io import BytesIO
 from PyPDF2 import PdfReader
 from rich import print
+from rich.console import Console
 
-
+console = Console()
 def save_pdf(content: bytes, filename: str, path: str) -> None:
     with open(f"{path}{filename}", "wb") as f:
         f.write(content)
 
 def get_links(query: str, results: int = 10, delay: int = 2) -> list[str]:
     dork = f"{query} filetype: pdf"
-    return [
-        l for l in search(dork, tld="co.in", num=results, stop=results, pause=delay)
-    ]
+    links = [l for l in search(dork, tld="co.in", num=results, stop=results, pause=delay)]
+    console.log("URLs found :mag:")
+    return links
 
 def get_bytes(links: list[str]) -> list[bytes]:
     contents = []
@@ -21,11 +22,11 @@ def get_bytes(links: list[str]) -> list[bytes]:
         try:
             r = requests.get(link, timeout=1)
             contype = r.headers["content-type"]
-            print(f"{link} {contype}")
             if "pdf" in contype:
                 contents.append(r.content)
         except:
             pass
+    console.log("URLs data taken :clipboard:")
     return contents
 
 
@@ -48,10 +49,12 @@ def get_info(contents: list[bytes]) -> list[dict]:
 
 
 def main():
-    book = input("Book name: ")
-    links = get_links(query=book, results=10)
-    bites = get_bytes(links)
-    infos = get_info(bites)
+    book = console.input("[magenta]Which book are you looking for today:books:?[/]: ")
+    print("\n")
+    with console.status("Searching for your books", spinner="moon"):
+        links = get_links(query=book, results=10)
+        bites = get_bytes(links)
+        infos = get_info(bites)
     for index, info in enumerate(infos):
         print(f"{index}.")
         for k, v in info.items():
